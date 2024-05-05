5008a42896429cf7dd5a215e52a1a622cb0654c7
hassannadeemansari <164927940+hassannadeemansari@users.noreply.github.com>
2024-05-05 10:12:14 +0500

    these are my first two files

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+//this my first coding project and i am very happy when i written this portfolio;
+let name = ('hassan');
+console.log("name");
---
+++ main.ts
@@ -0,0 +1,3 @@
+//this my first coding project and i am very happy when i written this portfolio;
+let name:string =('hassan');
+console.log("name");
